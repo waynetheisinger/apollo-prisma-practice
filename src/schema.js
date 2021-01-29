ee1a3e1cafb6613dd83932d6115c25a4c83f1efc
waynetheisinger <wayne@mowdirect.co.uk>
2021-01-29 12:44:07 +0000

    feature(resolvers): add

--- src/schema.js
+++ src/schema.js
@@ -37,3 +37,41 @@ const posts = [
     published: false,
   },
 ]
+const resolvers = {
+  Query: {
+    feed: (parent, args) => {
+      return posts.filter((post) => post.published)
+    },
+    post: (parent, args) => {
+      return posts.find((post) => post.id === Number(args.id))
+    },
+  },
+  Mutation: {
+    createDraft: (parent, args) => {
+      posts.push({
+        id: posts.length + 1,
+        title: args.title,
+        content: args.content,
+        published: false,
+      })
+      return posts[posts.length - 1]
+    },
+    publish: (parent, args) => {
+      const postToPublish = posts.find((post) => post.id === Number(args.id))
+      postToPublish.published = true
+      return postToPublish
+    },
+  },
+  Post: {
+    content: (parent) => parent.content,
+    id: (parent) => parent.id,
+    published: (parent) => parent.published,
+    title: (parent) => parent.title,
+  },
+}
+
+
+module.exports = {
+  resolvers,
+  typeDefs,
+}
